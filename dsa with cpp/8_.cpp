ed5f87738cd80663a094201bc248a1c63a1599fc
elliot <elliot@localhost.localdomain>
2023-01-22 18:09:36 +0530

    vault backup: 2023-01-22 18:09:36

--- dsa with cpp/8_.cpp
+++ dsa with cpp/8_.cpp
@@ -61,9 +61,11 @@ int fib(int n)
     int a = 0,b =1,sum = 0;
     for(int i = 1;i <= n;i++)
     {
+        cout<< sum << " ";
         a = b;
         b = sum;
         sum = a + b;
+        
     }
     return sum;
 }

cd70755aa75e8f7a81d3264226481e79fa654858
elliot <elliot@localhost.localdomain>
2023-01-22 17:17:07 +0530

    vault backup: 2023-01-22 17:17:07

---
+++ dsa with cpp/8_.cpp
@@ -0,0 +1,95 @@
+#include<iostream>
+using namespace std;
+
+bool isEven(int num)
+{
+    if(num%2==0)
+    {
+        return true;
+    }
+    else
+    return false;
+}
+
+int fact(int num)
+{
+
+    int ans = 1;
+    for(int i = 1;i <= num;i++)
+    {
+        ans = ans *i;
+        
+    }
+
+    return ans;
+}
+int nCr(int n,int r)
+{
+    return fact(n)/(fact(n-r)*fact(r));
+
+}
+int main()
+{
+    /*//switch case
+    int num = 2;
+    switch(num)
+    {
+        case 1:
+        cout << "First" << endl;
+        break;
+        case 2:
+        cout << "Second" << endl;
+        break;
+
+        default:
+        cout << "it is defualt case" << endl;  
+    }*/
+    
+    /*//calculator with operation selection
+    int a , b;
+    char op;
+    cout << "Enter Two Numbers :" << endl;
+    cin >> a >> b;
+    cout << "Enter the operation:" << endl;
+    cin >> op;
+    switch(op)
+    {
+        case '+':
+        cout << a+b << endl;
+        break;
+
+        case '-':
+        cout << a-b << endl;
+        break;
+        
+        case '*':
+        cout << a*b << endl;
+        break;
+
+        case '/':
+        cout << a/b << endl;
+        break;
+
+        default:
+        cout << "Invalid Case" << endl;
+    }
+    */
+
+    // functions 
+
+
+    //even odd
+    // int num ;
+    // cin >> num;
+    // cout << isEven(num) << endl;
+
+    // ncr
+    int n,r;
+    cin >> n>>r;
+    cout << nCr(n,r) << endl;
+    
+     
+
+    
+
+}
--- dsa with cpp/8_Switch Statement & Functions.md
+++ dsa with cpp/8_Switch Statement & Functions.md
@@ -12,8 +12,34 @@ switch(expression)//which evaluates to a integral value
 	case constant3:
 		statements;
 		break;
-	default:
+	default://defualt is optional
 		statements
 
 }
 ```
+- if we dont use break our other cases are also executed
+- we also can use nested switch cases
+
+## Continue in switch :
+in switch we cant use continue because it makes a error.
+## Functions :
+it is given a task which it can do multiple times by being called
+Syntax:
+```cpp
+return_type func_name(arguments)
+{
+	body
+
+	return return_type;
+}
+```
+
+
+## Programs made:
+- even checker
+- ncr calculator
+- prime checker
+
+## Function Call Stack :
+- functions are called in stack like plates in wedding which means (last come first serve)
+- 

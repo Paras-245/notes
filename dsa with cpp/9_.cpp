2344fcc154d2a17e3b703e4ec53f4a1ac59e318b
elliot <elliot@localhost.localdomain>
2023-01-23 18:50:57 +0530

    vault backup: 2023-01-23 18:50:57

--- dsa with cpp/9_.cpp
+++ dsa with cpp/9_.cpp
@@ -25,6 +25,21 @@ int getMax(int arr[],int n)
     return max;
 
 }
+
+int getMin(int arr[],int n)
+{
+    int min = INT_MAX;
+    for(int i = 0; i < n;i++)
+    {
+        if(arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+
+    return min;
+
+}
 int main()
 {
     // declare 

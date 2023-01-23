5181a39126a4b23b2ee8ea8d93b514c0baff02da
elliot <elliot@localhost.localdomain>
2023-01-23 18:49:57 +0530

    vault backup: 2023-01-23 18:49:57

--- dsa with cpp/9_.cpp
+++ dsa with cpp/9_.cpp
@@ -14,7 +14,16 @@ void printArray(int arr[],int size)
 int getMax(int arr[],int n)
 {
     int max = INT_MIN;
-    
+    for(int i = 0; i < n;i++)
+    {
+        if(arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+
+    return max;
+
 }
 int main()
 {

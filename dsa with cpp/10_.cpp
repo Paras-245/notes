63e65d074e8202c2f8ca2051821582726a601ef5
elliot <elliot@localhost.localdomain>
2023-01-24 17:27:01 +0530

    vault backup: 2023-01-24 17:27:01

--- dsa with cpp/10_.cpp
+++ dsa with cpp/10_.cpp
@@ -1,8 +1,19 @@
 #include<iostream>
 using namespace std;
+void printArray(int arr[],int size)
+{
+    cout << "Printing array"<< endl;
+    for(int i = 0; i < size;i++)
+    {
+        cout << arr[i] << ' ';
+    }
+    cout << "Printing Done"<< endl;
+
+}
 int main()
 {
     // Swap Alternate
     int arr[1,2,3,4,5,6,7];
+    print
     
 }

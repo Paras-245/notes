a1383a17ed20f755af124f4ab592fa142eaa5d98
elliot <elliot@localhost.localdomain>
2023-01-24 17:56:53 +0530

    vault backup: 2023-01-24 17:56:53

--- dsa with cpp/10_.cpp
+++ dsa with cpp/10_.cpp
@@ -34,10 +34,11 @@ int main()
     int ans = arr[0];
     for(int i =0; i < 8 ;i++)
     {
+        cout << ans << ' ';
         ans = ans ^ arr[i];
     }
 
-    cout << ans << endl;
+    // cout << ans << endl;
 
 
 

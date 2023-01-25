1b4d6f61d1ca8e83729bbd2fa1e0ff4afdcf262b
elliot <elliot@localhost.localdomain>
2023-01-25 17:43:03 +0530

    vault backup: 2023-01-25 17:43:02

--- dsa with cpp/10_.cpp
+++ dsa with cpp/10_.cpp
@@ -45,7 +45,7 @@ int main()
     */
 
    
-   // Intersection of arrays
+   /*/ Intersection of arrays
 //    int arr1[] = {1,2,3,4,5,6,7,8},arr2[] = {9,10,11,12,13,14};
 
 //    int intersection = 0;
@@ -67,6 +67,7 @@ int main()
 //         cout << -1 <<endl;
 //     }
     // int this case we are getting tle
+    */
     
 
    

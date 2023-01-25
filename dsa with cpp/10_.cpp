4c5fa07c406ddaa535011d85594aac9aedb7bd8d
elliot <elliot@localhost.localdomain>
2023-01-25 17:45:03 +0530

    vault backup: 2023-01-25 17:45:03

--- dsa with cpp/10_.cpp
+++ dsa with cpp/10_.cpp
@@ -70,8 +70,11 @@ int main()
     */
     
     // 3 SUM
-    int arr[] = {10, 5 ,5 ,5, 2 ,12};
-    for(int i == )
+    int arr[] = {10, 5 ,5 ,5, 2 ,12},target = 12;
+    for(int i =0 ;i < 5;i++)
+    {
+        for(j = i +1)
+    }
 
    
 

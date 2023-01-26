11a0d9c1e33dbb56aec1b0a8d8412c6133b0aace
elliot <elliot@localhost.localdomain>
2023-01-26 14:06:15 +0530

    vault backup: 2023-01-26 14:06:15

--- dsa with cpp/10_.cpp
+++ dsa with cpp/10_.cpp
@@ -151,7 +151,27 @@ int main()
 
     //Homework
     // 1. Unique number of occurences
-    int arr[] = {1,2,2,1,1,3},count[1000];
+    int arr[] = {1,2,2,1,1,3};
+    int i = 0;
+    sort(arr.begin(),arr.end());
+    vector<int> ans;
+    while (i < arr.size())
+    {
+        int count = 1;
+        for (int j = i+1; j< arr.size(); j++){
+            if (arr[i] == arr[j])
+                count++;
+        }
+            ans.push_back(count);
+            i = i + count;
+        }
+        sort(ans.begin(),ans.end());
+        for (int i = 0; i < ans.size() - 1; i++){
+            if (ans[i] == ans [i+1])
+                return false;
+        }
+        return true;
+    }
 
 
 

3181d29c7c3be9c3125f525486f32587b4399cc0
elliot <elliot@localhost.localdomain>
2023-01-27 14:43:22 +0530

    vault backup: 2023-01-27 14:43:22

--- 13_.cpp
+++ 13_.cpp
@@ -27,6 +27,34 @@ int firstOccurence(int arr[],int n,int key)
     return ans;
     
 }
+
+int firstOccurence(int arr[],int n,int key)
+{
+    int s = 0, e = n -1,ans = -1;
+    int mid = s+(e-s)/2;
+    while(s<=e)
+    {
+        if(arr[mid] == key)
+        {
+            ans = mid;
+            s = mid + 1;
+        }
+        else if (key > arr[mid]) // right
+        {
+            s = mid+1;
+
+        }
+        else if(key < arr[mid])// left 
+        {
+            e = mid-1;
+
+        }
+
+        mid = s+(e-s)/2;
+    }
+    return ans;
+    
+}
 int main()
 {
     int even[] = {1,2,3,3,5};

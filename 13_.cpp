1eb764fb66619d75f5d2fe500fac69bddbf76be6
elliot <elliot@localhost.localdomain>
2023-01-27 14:44:22 +0530

    vault backup: 2023-01-27 14:44:21

--- 13_.cpp
+++ 13_.cpp
@@ -28,7 +28,7 @@ int firstOccurence(int arr[],int n,int key)
     
 }
 
-int firstOccurence(int arr[],int n,int key)
+int lastOccurence(int arr[],int n,int key)
 {
     int s = 0, e = n -1,ans = -1;
     int mid = s+(e-s)/2;
@@ -58,7 +58,8 @@ int firstOccurence(int arr[],int n,int key)
 int main()
 {
     int even[] = {1,2,3,3,5};
-    cout << firstOccurence(even,5,3) << endl;; 
+    cout << firstOccurence(even,5,3) << endl;
+    cout << lastOccurence(even,5,3) << endl;
 
 
 }

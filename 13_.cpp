213b9e05cc6ff8b0f14f4ad4d6c347c3769274d7
elliot <elliot@localhost.localdomain>
2023-01-27 14:37:22 +0530

    vault backup: 2023-01-27 14:37:22

--- 13_.cpp
+++ 13_.cpp
@@ -2,24 +2,27 @@
 using namespace std;
 int firstOccurence(int arr[],int n,int key)
 {
-    int s = 0, e = n -1;
+    int s = 0, e = n -1,ans = -1;
     int mid = s+(e-s)/2;
     while(s<=e)
     {
         if(arr[mid] == key)
         {
-
+            ans = mid;
+            e = mid -1;
         }
-        else if (key > arr[mid])
+        else if (key > arr[mid]) // right
         {
 
         }
 
-        else if()
+        else if(key < arr[mid])
         {
 
         }
     }
+
+    mind = s+(e-s)/2;
 }
 int main()
 {

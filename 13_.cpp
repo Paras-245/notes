eb308fb74151c19ee9eaa887cf799b7d336cc423
elliot <elliot@localhost.localdomain>
2023-01-27 14:35:23 +0530

    vault backup: 2023-01-27 14:35:23

--- 13_.cpp
+++ 13_.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
 using namespace std;
-int firstOccurence(int arr[])
+int firstOccurence(int arr[],int n,int key)
+{
+    int s = 0, e = n -1;
+    int mid = s+(e-s)
+}
 int main()
 {
     int even[] = {1,2,3,3,5};

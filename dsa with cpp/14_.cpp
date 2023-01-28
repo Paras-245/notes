cb5c866f65321b1c9ced3c5d3f6da04707cd18a0
elliot <elliot@localhost.localdomain>
2023-01-28 13:16:35 +0530

    vault backup: 2023-01-28 13:16:35

--- dsa with cpp/14_.cpp
+++ dsa with cpp/14_.cpp
@@ -1,6 +1,12 @@
 #include<iostream>
 using namespace std;
+int getPivot(int arr[],int n)
+{
+
+}
 int main()
 {
-    
+    // find pivot in array
+    int arr[5] = {8,10,17,1,3};
+    cout << "Pivot is" 
 }

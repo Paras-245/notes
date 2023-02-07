7a6f0cd18914e5541d4bc0a4a6250acddb7b68ba
elliot <elliot@localhost.localdomain>
2023-02-07 18:35:37 +0530

    vault backup: 2023-02-07 18:35:37

--- dsa with cpp/20_.cpp
+++ dsa with cpp/20_.cpp
@@ -3,13 +3,19 @@
 using namespace std;
 void merge(int arr1[],int n, int arr2[],int m, int arr3[])
 {
-    int i =0,j=0;
+    int i =0,j=0,k=0;
     while(i < n && j <m)
     {
         if(arr1[i] < arr2[j])
         {
             arr3[k] = arr1[i];
             i++;
+            k++;
+        }
+        else if(arr1[i] > arr2[j])
+        {
+            arr3[k] = arr2[j];
+            
         }
     }
 }

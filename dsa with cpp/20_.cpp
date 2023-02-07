7e206dd7637222d563251c7d5fe3ba42228ffe08
elliot <elliot@localhost.localdomain>
2023-02-07 18:31:37 +0530

    vault backup: 2023-02-07 18:31:37

--- dsa with cpp/20_.cpp
+++ dsa with cpp/20_.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-void merge(int arr1[])
+void merge(int arr1[],int n, int arr2[],int m, int arr3[])
+{
+    
+}
 int main()
 {
     int arr1[5]= {1,3,5,7,9};

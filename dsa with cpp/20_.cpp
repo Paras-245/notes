0f2c1089b1f30c295c93e369ec335fae7f19da76
elliot <elliot@localhost.localdomain>
2023-02-07 18:42:42 +0530

    vault backup: 2023-02-07 18:42:41

--- dsa with cpp/20_.cpp
+++ dsa with cpp/20_.cpp
@@ -42,12 +42,15 @@ void print(int arr[], int n)
 }
 int main()
 {
-    int arr1[5] = {1, 3, 5, 7, 9};
-    int arr2[3] = {2, 4, 6};
-    int arr3[8] = {0};
+    // merge arrays
+    // int arr1[5] = {1, 3, 5, 7, 9};
+    // int arr2[3] = {2, 4, 6};
+    // int arr3[8] = {0};
+    // merge(arr1, 5, arr2, 3, arr3);
+    // print(arr3, 8);
 
-    merge(arr1, 5, arr2, 3, arr3);
-    print(arr3, 8);
+    // move zeros
+    
 
     return 0;
 }

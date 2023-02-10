729d30aff9974d6ab4760eb9ac7754c2e463f4ba
elliot <elliot@localhost.localdomain>
2023-02-10 18:44:31 +0530

    vault backup: 2023-02-10 18:44:30

--- dsa with cpp/22_.cpp
+++ dsa with cpp/22_.cpp
@@ -1,10 +1,16 @@
 #include<iostream>
 using namespace std;
+int getLength(char name[])
+{
+    for(int i = 0;)
+}
 int main()
 {
     char name[20];
     cin >> name;
-    cout <<name;
+    // cout <<name;
     
+    // length of string
+
     return 0;
 }

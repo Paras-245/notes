c80498553c5ff85bdc3aa7d31a1c65b9ba4404c4
elliot <elliot@localhost.localdomain>
2023-02-14 18:41:36 +0530

    vault backup: 2023-02-14 18:41:36

--- dsa with cpp/23_.cpp
+++ dsa with cpp/23_.cpp
@@ -24,14 +24,19 @@ int main()
     // }
 
     // output 
-    // for(int i = 0; i<3;i++)
-    // {
-    //     for(int j = 0; j < 4;j++)
-    //     {
-    //         cout << arr[i][j] << ' ';
-    //     }
-    //     cout << endl;
-    // }
+    for(int i = 0; i<3;i++)
+    {
+        for(int j = 0; j < 4;j++)
+        {
+            cout << arr[i][j] << ' ';
+        }
+        cout << endl;
+    }
+
+    cout << "Enter the element to search " << endl;
+    int target ;
+    cin >>target;
+    
 
 
     return 0;

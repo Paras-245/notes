8b1fa4409d25f894fd76120e669fddf03515726e
elliot <elliot@localhost.localdomain>
2023-02-15 20:03:59 +0530

    vault backup: 2023-02-15 20:03:59

--- dsa with cpp/23_.cpp
+++ dsa with cpp/23_.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 #include<limits.h>
 using namespace std;
-bool isPresent(int arr[][4],int target,int row ,int col)
+bool isPresent(int arr[][3],int target,int row ,int col)
 {
     for(int row = 0; row<3;row++)
     {
-        for(int col = 0; col < 4;col++)
+        for(int col = 0; col < 3;col++)
         {
             if(arr[row][col] == target)
             {
@@ -18,12 +18,12 @@ bool isPresent(int arr[][4],int target,int row ,int col)
 
 }
 
-void printSum(int arr[][4],int row,int col)
+void printSum(int arr[][3],int row,int col)
 {
     for(int row = 0; row<3;row++)
     {
         int sum = 0;
-        for(int col = 0; col < 4;col++)
+        for(int col = 0; col < 3;col++)
         {
             sum+=arr[row][col];
         }
@@ -31,14 +31,14 @@ void printSum(int arr[][4],int row,int col)
     }
     
 }
-void largestRowSum(int arr[][4],int row,int col)
+void largestRowSum(int arr[][3],int row,int col)
 {
     int max = INT_MIN;
     int num = -1;
     for(int row = 0; row<3;row++)
     {
         int sum = 0;
-        for(int col = 0; col < 4;col++)
+        for(int col = 0; col < 3;col++)
         {
             sum+=arr[row][col];
         }
